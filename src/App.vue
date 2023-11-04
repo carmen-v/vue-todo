fdd12988004c884abb5336c66999bacb1e18723d
CarmenMihaela <71848618+CarmenMihaela@users.noreply.github.com>
2023-11-04 16:12:00 +0100

    add TodosHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import TodosHeader from './components/TodosHeader.vue';
 
 </script>
 
 <template>
+  <TodosHeader />
 
   <RouterView />
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Nunito&family=Sora&display=swap');
+
+* {
+  font-family: "Rubik", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+  
+  .branding {
+    display: flex;
+    align-items: center;
+  }
+  img {
+    width: 100px;
+  }
+}
+</style>
---
+++ src/components/TodosHeader.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { RouterLink } from "vue-router"</script>
+<template>
+    <header>
+        <nav class="container">
+            <div class="branding">
+                <img src="../assets/bird_logo.png" alt="bird logo of todo app" />
+                <h1>To-Do List Web App</h1>
+            </div>
+            <ul class="nav-routes">
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+
+<style lang="scss" scoped>
+header {
+  background-color: #f1f1f1;
+  nav {
+    display: flex;
+    align-items: center;
+    padding: 25px 16px;
+
+    .branding {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+      img {
+        max-width: 50px;
+      }
+
+      h1 {
+        font-size: 24px;
+      }
+    }
+
+    .nav-routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      gap: 12px;
+      list-style: none;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
+}
+</style>
