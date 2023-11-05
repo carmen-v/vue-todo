3f398b742d7eaeb93e1f3a07efd0d582e232a484
CarmenMihaela <71848618+CarmenMihaela@users.noreply.github.com>
2023-11-05 14:24:01 +0100

    add from the api

--- src/App.vue
+++ src/App.vue
@@ -6,30 +6,9 @@ import TodosHeader from './components/TodosHeader.vue';
 
 <template>
   <TodosHeader />
-
   <RouterView />
 </template>
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Nunito&family=Sora&display=swap');
-
-* {
-  font-family: "Rubik", sans-serif;
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-.container {
-  max-width: 1100px;
-  margin: 0 auto;
-  
-  .branding {
-    display: flex;
-    align-items: center;
-  }
-  img {
-    width: 100px;
-  }
-}
 </style>
--- src/views/TodosView.vue
+++ src/views/TodosView.vue
@@ -77,38 +77,3 @@ const deleteTodo = (todoId) => {
     <TodoForm @create-todo="createTodo" />
   </main>
 </template>
-
-<style lang="scss" scoped>
-main {
-  display: flex;
-  flex-direction: column;
-  max-width: 500px;
-  width: 100%;
-  margin: 0 auto;
-  padding: 40px 16px;
-
-  h1 {
-    margin-bottom: 16px;
-    text-align: center;
-    background: -webkit-linear-gradient(#db0eaf, #356480);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  }
-
-  .todo-list {
-    display: flex;
-    flex-direction: column;
-    list-style: none;
-    margin-top: 24px;
-    gap: 20px;
-  }
-
-  .todos-msg {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 8px;
-    margin-top: 24px;
-  }
-}
-</style>
